57305824275fe6ae9342f8d9ce87eb16a672e57a
becky <becky@aiodin.com>
2025-07-09 18:39:54 +0800

    - Fixed bugs

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
     baseUrl: 'https://ibc22bo.porsche68.com/login/auth', // Or your target site
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.ts',
-    projectId: "eynnyd"
+    projectId: "eynnyd",
+    chromeWebSecurity: false
   }
   
 });
--- cypress/e2e/shadow-login.cy.ts
+++ cypress/e2e/shadow-login.cy.ts
@@ -19,15 +19,12 @@ describe('Shadow Login', () => {
         cy.url().should('include', '/general/members');
         cy.get('.kt-header-mobile__toolbar > #kt_aside_toggler').click();
         cy.get('button.btn.btn-success.btn-icon-sm').click();
-            //     cy.get('table.table-sortable tbody tr').each(($row) => {
-            //     cy.wrap($row).find('td').eq(0).invoke('text').then((text) => {
-        
-            // });
         cy.get('input[formcontrolname="username"]').type('winter01');
         cy.get('button.btn.btn-success.btn-icon-sm').click();
         cy.wait(5000);
         cy.get('table.table-sortable').parent().scrollTo('right');
         cy.get('[ng-reflect-message="Shadow Login"] > .fas').click();
+        
         cy.get('body').then($body => {
             if ($body.find('.swal2-popup:visible').length > 0) {
                 cy.window().then((win) => {
@@ -40,21 +37,19 @@ describe('Shadow Login', () => {
                 .and('contain', 'Yes')
                 .invoke('removeAttr', 'target') 
                 .click();
-            } else {
+                cy.wait(3000);
+                cy.origin('https://ibc22.porsche68.com', () => {
+                    cy.url({ timeout: 10000 }).should('include', 'access_token');
+                    
+                    cy.get('a[href="/member/profile"]', { timeout: 10000 })
+                    .should('be.visible')
+                    .and('contain', 'MY PROFILE');
+                    });
+                } else {
                 cy.log('No SweetAlert2 popup appeared.');
-            }
+                }
         });
-        cy.wait(10000);
-        // cy.origin('https://ibc22.porsche68.com', () => {
-        //     cy.location('search', { timeout: 1000 }).should('include', 'access_token');
-        // });
-        cy.get('a[href="/member/profile"]')
-        .should('be.visible')
-        .and('contain', 'MY PROFILE');
     });
-
-        // Interact with the Actions column in the first row
-        //cy.get(':nth-child(1) > .actions-column', { timeout: 10000 }).should('be.visible').click(); // Example action; adjust as needed
 });
         
   
