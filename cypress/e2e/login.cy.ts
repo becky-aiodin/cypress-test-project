1c0f97e8084f5a9cc15e931eea0f50a3b4ea5664
becky <becky@aiodin.com>
2025-07-10 14:51:57 +0800

    - Added login

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,25 @@
+describe('Login', () => {
+    const baseUrl = 'https://ibc22.porsche68.com/';
+    const username = 'testcheck01'
+    const password = '123123'
+    
+    it('Registers a new user and logs in successfully', () => {
+        cy.visit('/');
+        cy.document().its('readyState').should('eq', 'complete');
+        cy.window().should('have.property', 'location');
+
+        cy.get('button.main-button-2').contains('LOGIN').click();
+        cy.wait(1000);
+        cy.get('input[formcontrolname="username"]').type(username);
+        cy.wait(1000);
+        cy.get('input[formcontrolname="password"]').type(password);
+        cy.wait(1000);
+        cy.get('button.main-button-1.button-height').contains('LOGIN').click();
+        cy.wait(5000);
+
+        cy.get('a[href="/member/profile"]', { timeout: 10000 }).should('be.visible').and('contain', 'MY PROFILE');
+        cy.contains('a', 'LOGOUT', { timeout: 10000 }).should('be.visible');
+    });
+});
+        
+  
